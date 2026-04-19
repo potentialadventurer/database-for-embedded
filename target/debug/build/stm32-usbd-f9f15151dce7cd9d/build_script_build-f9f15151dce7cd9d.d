0d8739fea49124c3a4a6289842fc2732a8d4b6ec
potentialadventurer <143354532+potentialadventurer@users.noreply.github.com>
2026-04-18 21:24:58 -0400

    update

---
+++ target/debug/build/stm32-usbd-f9f15151dce7cd9d/build_script_build-f9f15151dce7cd9d.d
@@ -0,0 +1,5 @@
+/home/roninfrompi/Documents/GitHub/database-for-embedded/target/debug/build/stm32-usbd-f9f15151dce7cd9d/build_script_build-f9f15151dce7cd9d.d: /home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32-usbd-0.6.0/build.rs
+
+/home/roninfrompi/Documents/GitHub/database-for-embedded/target/debug/build/stm32-usbd-f9f15151dce7cd9d/build_script_build-f9f15151dce7cd9d: /home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32-usbd-0.6.0/build.rs
+
+/home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32-usbd-0.6.0/build.rs:
---
+++ target/debug/build/stm32f1-e5261478ccf8b5c6/build_script_build-e5261478ccf8b5c6.d
@@ -0,0 +1,5 @@
+/home/roninfrompi/Documents/GitHub/database-for-embedded/target/debug/build/stm32f1-e5261478ccf8b5c6/build_script_build-e5261478ccf8b5c6.d: /home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32f1-0.15.1/build.rs
+
+/home/roninfrompi/Documents/GitHub/database-for-embedded/target/debug/build/stm32f1-e5261478ccf8b5c6/build_script_build-e5261478ccf8b5c6: /home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32f1-0.15.1/build.rs
+
+/home/roninfrompi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stm32f1-0.15.1/build.rs:
